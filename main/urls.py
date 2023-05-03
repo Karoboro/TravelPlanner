da6eeac55c2ab44f5b3c07c0161ae78ed3f28c55
HenryChang.laptop <henrychang01713@gmail.com>
2023-05-02 20:56:18 -0700

    edit trip and delete trip functionality

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("create_trip/", views.create_trip, name="create_trip"),
     path("create_day/", views.create_day, name="create_day"),
     path("create_event/", views.create_event, name="create_event"),
+    path("edit/<int:trip_id>/", views.edit_trip, name="edit_trip"),
+    path("edit/<int:event_id>/", views.edit_event, name="edit_event"),
+    path("delete/<int:trip_id>", views.delete_trip, name="delete_trip"),
 ]
