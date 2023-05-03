e7982ccaf28cba17ddffdc75fff4c0f399d6fcac
HenryChang.laptop <henrychang01713@gmail.com>
2023-05-02 18:55:14 -0700

    constraint added for Day model, __len__ added for Trip and Day, added temporary view to create event

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 # from django.contrib.auth.models import User
+from datetime import date
+
 from django.core.validators import MinValueValidator
 from django.db import models
 
@@ -7,10 +9,14 @@ from django.db import models
 class Trip(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+    start_date = models.DateField("Start Date", default=date.today)
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+    def __len__(self):
+        return self.day_set.count()
 
 
 class Day(models.Model):
@@ -26,20 +32,29 @@ class Day(models.Model):
     def __str__(self):
         return str(self.num)
 
+    def __len__(self):
+        return self.event_set.count()
+
 
 class Event(models.Model):
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(cost__gte=0), name="cost_gte_0")
+        ]
+
     CATEGORY_LIST = [
         ("Accommodation", "Accommodation"),
         ("Transportation", "Transportation"),
         ("Entertainment", "Entertainment"),
         ("Food", "Food"),
+        ("Misc", "Misc"),
     ]
     day = models.ForeignKey(Day, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200, choices=CATEGORY_LIST)
     time = models.TimeField("Start Time")
     location = models.CharField(max_length=200)
-    cost = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    cost = models.FloatField(default=0, validators=[MinValueValidator(0)])
     description = models.CharField(max_length=200)
 
     def __str__(self):
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views.generic.edit import UpdateView
 
 from .forms import DayForm, EventForm, TripForm
 from .models import Trip
@@ -33,3 +34,14 @@ def create_day(request):
         form = DayForm()
 
     return render(request, "main/create_day.html", {"form": form})
+
+
+def create_event(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = EventForm()
+
+    return render(request, "main/create_event.html", {"form": form})
