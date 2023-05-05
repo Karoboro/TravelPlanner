8dd655ce041370dfca0b5596e45b27385b78a9dc
Henry Chang PC <henrychang0713@gmail.com>
2023-05-05 03:51:31 -0700

    added widget and fields to forms.py for input validation

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,9 @@
+from typing import Any, Dict, Mapping, Optional, Type, Union
+
 from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 
 from .models import Day, Event, Trip
 
@@ -12,6 +17,8 @@ class TripForm(forms.ModelForm):
 
 
 class DayForm(forms.ModelForm):
+    trip = forms.ModelChoiceField(queryset=Trip.objects.all(), empty_label=None)
+
     class Meta:
         model = Day
         fields = ["trip", "num"]
@@ -20,4 +27,9 @@ class DayForm(forms.ModelForm):
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ["day", "name", "category", "time", "location", "cost", "description"]
+        fields = ["name", "category", "time", "location", "cost", "description"]
+        widgets = {
+            "time": forms.TimeInput(attrs={"type": "time"}),
+            "cost": forms.NumberInput(attrs={"Step": 0.01, "min": 0}),
+            "description": forms.Textarea(attrs={"rows": 4}),
+        }
