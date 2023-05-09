c4d1796f9abbd2319e282d61afbbc9639566293c
Henry Chang PC <henrychang0713@gmail.com>
2023-05-08 22:38:49 -0700

    test __len__ for Trip model

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,16 @@
+from django.db.utils import IntegrityError
 from django.test import TestCase
+from django.test.utils import setup_test_environment
+
+from .models import Day
+
 
 # Create your tests here.
+class TripModelTests(TestCase):
+    def test_number_of_days(self):
+        trip = Trip(name="Trip to Somewhere", description="A testing trip")
+        trip.save()
+        trip.day_set.create(num=1)
+        trip.day_set.create(num=2)
+        trip.day_set.create(num=3)
+        self.assertIs(len(trip), 3)
