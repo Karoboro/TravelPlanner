135e2c029c1a6567833c74a87837e35560b0c946
Henry Chang PC <henrychang0713@gmail.com>
2023-05-10 09:48:21 -0700

    changed ALLOWED_HOST in setting to allow for testserver, tested setUp for fixture in test

--- main/tests.py
+++ main/tests.py
@@ -7,9 +7,11 @@ from .models import Trip
 
 # Create your tests here.
 class TripModelTests(TestCase):
+    def setUp(self):
+        Trip.objects.create(name="Trip to Somewhere", description="A testing trip")
+
     def test_number_of_days(self):
-        trip = Trip(name="Trip to Somewhere", description="A testing trip")
-        trip.save()
+        trip = Trip.objects.get(name="Trip to Somewhere")
         trip.day_set.create(num=1)
         trip.day_set.create(num=2)
         trip.day_set.create(num=3)
@@ -17,21 +19,18 @@ class TripModelTests(TestCase):
 
     def test_invalid_duplicate_day(self):
         with self.assertRaises(IntegrityError):
-            trip = Trip(name="Trip to Somewhere", description="A testing trip")
-            trip.save()
+            trip = Trip.objects.get(name="Trip to Somewhere")
             trip.day_set.create(num=1)
             trip.day_set.create(num=1)
 
     def test_invalid_neg_day_num(self):
         with self.assertRaises(IntegrityError):
-            trip = Trip(name="Trip to Somewhere", description="A testing trip")
-            trip.save()
+            trip = Trip.objects.get(name="Trip to Somewhere")
             trip.day_set.create(num=-1)
 
     def test_invalid_event_cost(self):
         with self.assertRaises(IntegrityError):
-            trip = Trip(name="Trip to Somewhere", description="A testing trip")
-            trip.save()
+            trip = Trip.objects.get(name="Trip to Somewhere")
             day = trip.day_set.create(num=1)
             day.save()
             day.event_set.create(
