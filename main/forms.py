12b6168fd9ec41ac2271fc8be96f8d3029ab664a
Henry Chang PC <henrychang0713@gmail.com>
2023-05-15 20:36:17 -0700

    fix incorrect fields for UserForm

--- main/forms.py
+++ main/forms.py
@@ -8,7 +8,7 @@ from .models import Day, Event, Trip
 class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "email", "firstname", "lastname"]
+        fields = ["username", "email", "first_name", "last_name"]
         field_classes = {"username": UsernameField}
 
 
