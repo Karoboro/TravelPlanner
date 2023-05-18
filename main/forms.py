5dbaa81958fe3efbee50b0329406f0fe95b5892f
Henry Chang PC <henrychang0713@gmail.com>
2023-05-17 21:33:35 -0700

    changed wording to password reset in password reset done page, removed repeating day attr in forms

--- main/forms.py
+++ main/forms.py
@@ -7,21 +7,27 @@ from .models import Day, Event, Trip
 
 
 class UserForm(UserCreationForm):
-    username = forms.EmailField(widget=forms.TextInput(attrs={"class": "form-control my-3"}))
+    username = forms.EmailField(
+        widget=forms.TextInput(attrs={"class": "form-control my-3"})
+    )
 
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name"]
         widgets = {
-            # "username": forms.TextInput(attrs={"class": "form-control"}), # doesn't work 
-            "first_name": forms.TextInput(attrs={"class": "form-control my-3"}), 
+            # "username": forms.TextInput(attrs={"class": "form-control"}), # doesn't work
+            "first_name": forms.TextInput(attrs={"class": "form-control my-3"}),
             "last_name": forms.TextInput(attrs={"class": "form-control my-3"}),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["password1"].widget = PasswordInput(attrs={"class": "form-control my-3"})
-        self.fields["password2"].widget = PasswordInput(attrs={"class": "form-control my-3"})
+        self.fields["password1"].widget = PasswordInput(
+            attrs={"class": "form-control my-3"}
+        )
+        self.fields["password2"].widget = PasswordInput(
+            attrs={"class": "form-control my-3"}
+        )
 
 
 class TripForm(forms.ModelForm):
@@ -31,9 +37,12 @@ class TripForm(forms.ModelForm):
         fields = ["name", "start_date", "description"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control"}),
-            "start_date": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+            "start_date": forms.DateInput(
+                attrs={"type": "date", "class": "form-control"}
+            ),
             "description": forms.Textarea(attrs={"class": "form-control"}),
         }
+
 
 class DayForm(forms.ModelForm):
     trip = forms.ModelChoiceField(queryset=Trip.objects.all(), empty_label=None)
@@ -48,12 +57,15 @@ class EventForm(forms.ModelForm):
         model = Event
         fields = ["day", "name", "category", "time", "location", "cost", "description"]
         widgets = {
-            "day": forms.Select(attrs={'class': 'form-control'}),
             "day": forms.HiddenInput(),
-            "name": forms.TextInput(attrs={'class': 'form-control'}),
-            "category": forms.Select(choices=Event.CATEGORY_LIST, attrs={'class': 'form-control'}),            "time": forms.TimeInput(attrs={"type": "time", 'class': 'form-control'}),
-            "location": forms.TextInput(attrs={'class': 'form-control'}),
-            "cost": forms.NumberInput(attrs={"Step": 0.01, "min": 0, 'class': 'form-control'}),
-            "description": forms.Textarea(attrs={"rows": 4, 'class': 'form-control'}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "category": forms.Select(
+                choices=Event.CATEGORY_LIST, attrs={"class": "form-control"}
+            ),
+            "time": forms.TimeInput(attrs={"type": "time", "class": "form-control"}),
+            "location": forms.TextInput(attrs={"class": "form-control"}),
+            "cost": forms.NumberInput(
+                attrs={"Step": 0.01, "min": 0, "class": "form-control"}
+            ),
+            "description": forms.Textarea(attrs={"rows": 4, "class": "form-control"}),
         }
-
